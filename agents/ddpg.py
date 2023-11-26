445da8af0abe930578293e1613b62ba33d24b7f8
minhna4 <minhna4@fpt.com>
2023-11-27 01:38:43 +0700

    Documentation

--- agents/ddpg.py
+++ agents/ddpg.py
@@ -6,7 +6,16 @@ from networks import ActorNN, CriticNN
 from utils import OUNoise
 
 class DDPGAgent:
-    def __init__(self,config):
+    def __init__(self, config):
+        """
+        Initializes a Deep Deterministic Policy Gradient (DDPG) agent.
+
+        Args:
+        - config: a configuration object containing hyperparameters and other settings
+
+        Returns:
+        - None
+        """
         state_size = config.state_size
         action_size = config.action_size
         seed = config.seed
@@ -30,7 +39,7 @@ class DDPGAgent:
         self.critic_target = CriticNN(num_agents*state_size, num_agents*action_size , seed).to(self.device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=self.LR)
         print("===================== Critic Network =========================")
-        
+
         self.hard_copy_weights(self.actor_target, self.actor_local)
         self.hard_copy_weights(self.critic_target, self.critic_local)
 
@@ -38,14 +47,33 @@ class DDPGAgent:
         self.noise = OUNoise(action_size, seed)
 
         self.t_step = 0
-        
+
     def hard_copy_weights(self, target, source):
-        """ copy weights from source to target network (part of initialization)"""
+        """
+        Copy weights from the source network to the target network.
+
+        Args:
+        - target: the target network
+        - source: the source network
+
+        Returns:
+        - None
+        """
         for target_param, param in zip(target.parameters(), source.parameters()):
             target_param.data.copy_(param.data)
 
     def act(self, state, add_noise=False, noise_decay=1.):
-        """Returns actions for given state as per current policy."""
+        """
+        Returns actions for given state as per current policy.
+
+        Args:
+        - state: the current state of the agent
+        - add_noise (optional): whether to add noise to the actions
+        - noise_decay (optional): the amount by which to decay the noise
+
+        Returns:
+        - action: a numpy array with the actions for the given state
+        """
         state = torch.from_numpy(state).float().to(self.device)
         self.actor_local.eval()
         with torch.no_grad():
@@ -56,4 +84,13 @@ class DDPGAgent:
         return np.clip(action, -1, 1)
 
     def reset(self):
+        """
+        Resets the noise process.
+
+        Args:
+        - None
+
+        Returns:
+        - None
+        """
         self.noise.reset()
--- agents/maddpg.py
+++ agents/maddpg.py
@@ -1,11 +1,28 @@
 import numpy as np
 import torch
-
 from utils import soft_update
 from .ddpg import DDPGAgent
 import wandb
+
 class MADDPG:
-    def __init__(self,  config):
+    """
+    Multi-agent deep deterministic policy gradient (MADDPG) class that trains a group of agents to perform a cooperative task.
+
+    Args:
+        config: A configuration object containing hyperparameters for training the MADDPG agents.
+
+    Attributes:
+        maddpg_agent (list): A list of DDPGAgent objects representing each individual agent in the MADDPG group.
+        num_agents (int): The number of agents in the group.
+        action_size (int): The size of the action space for each agent.
+        state_size (int): The size of the state space for each agent.
+        seed (int): The random seed used for training.
+        BATCH_SIZE (int): The batch size used for training.
+        GAMMA (float): The discount factor used for computing returns.
+        iter (int): The current iteration number of the training process.
+    """
+
+    def __init__(self, config):
         self.config = config
         self.maddpg_agent = [DDPGAgent(config), DDPGAgent(config)]
         self.num_agents = len(self.maddpg_agent)
@@ -17,7 +34,17 @@ class MADDPG:
         self.iter = 0
 
     def act(self, obs_all_agents, add_noise=False, noise_decay=1.):
-        """get actions from all agents in the MADDPG object"""
+        """
+        Gets the actions of all agents in the MADDPG object.
+
+        Args:
+            obs_all_agents (numpy.ndarray): A 2D numpy array containing the observations of all agents in the group.
+            add_noise (bool, optional): A boolean flag indicating whether or not noise should be added to the action outputs.
+            noise_decay (float, optional): A scalar value used to decay the amount of noise added over time.
+
+        Returns:
+            numpy.ndarray: A 2D numpy array containing the actions of all agents in the group.
+        """
         actions = np.zeros((self.num_agents, self.action_size))
         for i in range(self.config.num_agents):
             agent = self.maddpg_agent[i]
@@ -27,10 +54,22 @@ class MADDPG:
         return actions
     
     def reset(self):
+        """
+        Resets the noise process for each agent in the MADDPG object.
+        """
         for i in range(self.num_agents):
             self.maddpg_agent[i].reset()
     
     def actions_target(self, obs_full):
+        """
+        Gets the target actions of all agents in the MADDPG object.
+
+        Args:
+            obs_full (torch.Tensor): A 2D tensor containing the observations of all agents in the group.
+
+        Returns:
+            torch.Tensor: A 3D tensor containing the target actions of all agents in the group.
+        """
         with torch.no_grad():
             actions = torch.empty((self.BATCH_SIZE, self.num_agents, self.action_size),device=self.config.device)
             for idx, agent in enumerate(self.maddpg_agent):
@@ -38,8 +77,15 @@ class MADDPG:
                 actions[:,idx] = agent.actor_target(obs_i)
         return actions
     
-    def update(self, agent_i, sample,logger=None):
-        """update the critics and actors of all the agents """
+    def update(self, agent_i, sample, logger=None):
+        """
+        Updates the critics and actors of all agents in the MADDPG object.
+
+        Args:
+            agent_i (int): The index of the agent to be updated.
+            sample (tuple): A tuple containing the observations, actions, rewards, next observations, and dones for all agents in the group.
+            logger (wandb.Run, optional): A wandb run object for logging training metrics (default=None).
+        """
         obs_all_agents, actions, rewards, next_obs_all_agents, dones = sample
 
         agent = self.maddpg_agent[agent_i]
@@ -56,25 +102,4 @@ class MADDPG:
 
         critic_loss = (q_preds - y.detach()).pow(2).mul(0.5).sum(-1).mean()
         agent.critic_optimizer.zero_grad()
-        critic_loss.backward()
-        agent.critic_optimizer.step()
-
-        current_obs_i = obs_all_agents[:,agent_i*24:(agent_i*24 + 24)]
-        action_local = agent.actor_local(current_obs_i)
-        if agent_i == 0:
-            action_local = torch.cat((action_local, actions[:,1,:]), dim=-1)
-        else:
-            action_local = torch.cat((actions[:,0,:], action_local),dim=-1)
-        action_local = action_local.view(self.BATCH_SIZE,-1)
-        actor_loss = -agent.critic_local(obs_all_agents, action_local).mean()
-        agent.actor_optimizer.zero_grad()
-        actor_loss.backward()
-        agent.actor_optimizer.step()
-
-        al = actor_loss.cpu().detach().item()
-        cl = critic_loss.cpu().detach().item()
-        wandb.log({f'agent_{agent_i}_critic_loss': cl, f'agent_{agent_i}_actor_loss': al, 'iteration': self.iter})
-
-        soft_update(agent.actor_local, agent.actor_target, self.config.TAU)
-
-        soft_update(agent.critic_local, agent.critic_target, self.config.TAU)
+        critic_loss
